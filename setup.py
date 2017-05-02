0530e99f755542a0709f6bcf495f076fd9b4c61a
Thor77 <thor77@thor77.org>
2017-05-02 23:45:55 +0200

    Add more metadata to setup.py
    * Add author, author_email, description, keywords and url

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup
 setup(
     name='ts3cli',
     version='0.1.0',
+    author='Thor77',
+    author_email='thor77@thor77.org',
+    description='CLI for Teamspeak 3\'s query interface',
+    keywords='ts3 teamspeak teamspeak3 ts3cli cli',
+    url='https://github.com/Thor77/TS3CLI',
     packages=['ts3cli'],
     install_requires=[
         'Click',
